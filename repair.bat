a7a8de91e899a945b9effaf78bf4535e9d15fb5b
abbaccadd0 <abbaccadd0@qq.com>
2023-06-05 21:43:36 +0800

    update

---
+++ repair.bat
@@ -0,0 +1,20 @@
+@echo off
+
+@REM 开始获取管理员权限
+setlocal
+set uac=~uac_permission_tmp_%random%
+md "%SystemRoot%\system32\%uac%" 2>nul
+if %errorlevel%==0 ( rd "%SystemRoot%\system32\%uac%" >nul 2>nul ) else (
+    echo set uac = CreateObject^("Shell.Application"^)>"%temp%\%uac%.vbs"
+    echo uac.ShellExecute "%~s0","","","runas",1 >>"%temp%\%uac%.vbs"
+    echo WScript.Quit >>"%temp%\%uac%.vbs"
+    "%temp%\%uac%.vbs" /f
+    del /f /q "%temp%\%uac%.vbs" & exit )
+
+ipconfig /flushdns
+netsh int ip reset
+netsh winsock reset
+bcdedit /set useplatformclock false
+DISM.exe /Online /Cleanup-image /Restorehealth
+sfc /scannow
+pause
