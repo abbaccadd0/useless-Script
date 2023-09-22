ea46022c736463a737ba884924c4c5b4364346fe
abbaccadd0 <abbaccadd0@live.com>
2023-09-22 14:26:57 +0800

    update files

--- repair.bat
+++ repair.bat
@@ -11,22 +11,19 @@ if %errorlevel%==0 ( rd "%SystemRoot%\system32\%uac%" >nul 2>nul ) else (
     "%temp%\%uac%.vbs" /f
     del /f /q "%temp%\%uac%.vbs" & exit )
 
-sc stop "wsearch" && sc config "wsearch" start=disabled
-sc delete QPCore
-ipconfig /flushdns
-netsh int ip reset
-netsh winsock reset
+sc stop "wsearch" & sc config "wsearch" start=disabled & sc start "DoSvc" & sc config "DoSvc" start=auto & sc delete QPCore
+ipconfig /flushdns & netsh int ip reset & netsh winsock reset
 netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
 netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2
 netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\DoSvc" /v Start /t REG_DWORD /d 2 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverrideMask /t REG_DWORD /d 3 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d 33554435 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v DisableTsx /t REG_DWORD /d 00000000 /f
-bcdedit /deletevalue useplatformclock
-bcdedit /deletevalue disabledynamictick
-@REM pause
+bcdedit /deletevalue useplatformclock & bcdedit /deletevalue disabledynamictick
+pause
 DISM.exe /Online /Cleanup-image /Restorehealth
 sfc /scannow
 pause
